545276332b6db319707ca373146e480b80249e8b
Chris DeLeon <gamkedo@gmail.com>
2016-05-19 16:09:13 -0700

    Got in placeholder/early turret from Angelo (turns to face player, doesn't yet attack or get destroyed), added more building placeholder material styles, turned off clouds for now (some sort of error, not sure what's causing it), adjusted cannonball material, made giant robot invulnerable for now

---
+++ Assets/Scripts/TurnToFacePlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnToFacePlayer : MonoBehaviour {
+	Vector3 aimTargetPos;
+	// Use this for initialization
+	void Start () {
+		aimTargetPos = Camera.main.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(aimTargetPos);
+	}
+
+	void FixedUpdate() {
+		float chaseK = 0.992f;
+
+		aimTargetPos = aimTargetPos * chaseK +
+			Camera.main.transform.position * (1.0f - chaseK);
+		aimTargetPos.y = transform.position.y; // to remain planer in base pivot
+	}
+}
