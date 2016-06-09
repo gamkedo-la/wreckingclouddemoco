65c84fa7c96b5d1080463d3e9407404147bdc338
Chris DeLeon <gamkedo@gmail.com>
2016-06-09 12:23:45 -0700

    Story intro text for narrative framing, camera movement for intro, title screen with start button

---
+++ Assets/Scripts/TimeStretch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeStretch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0.8f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
