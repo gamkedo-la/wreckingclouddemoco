0751381418ba0d9b4d0c2445c640ad79d913193f
Chris DeLeon <gamkedo@gmail.com>
2016-06-19 06:44:35 -0700

    Giant enemy robot now simulates walking through city, autofire added to player weapons, help text updated to reflect newer controls when in flight or attacking

--- Assets/Scripts/CannonFire.cs
+++ Assets/Scripts/CannonFire.cs
@@ -6,6 +6,10 @@ public class CannonFire : MonoBehaviour {
 	public GameObject spawnAttackPrefab;
 	Transform fireFrom;
 
+	public bool autoFire = false;
+	public float reloadDelay = 0.75f;
+	float reloadLeft = 0.0f;
+
 	// Use this for initialization
 	void Start () {
 		fireFrom = transform.Find("FireFrom");
@@ -13,8 +17,14 @@ public class CannonFire : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(triggerKey)) {
+		if(reloadLeft > 0.0f) {
+			reloadLeft -= Time.deltaTime;
+		}
+		if( reloadLeft <= 0.0f &&
+			( (autoFire==false && Input.GetKeyDown(triggerKey)) ||
+				(autoFire && Input.GetKey(triggerKey)) ) ) {
 			GameObject.Instantiate(spawnAttackPrefab, fireFrom.position, fireFrom.rotation);
+			reloadLeft += reloadDelay;
 		}
 	}
 }
