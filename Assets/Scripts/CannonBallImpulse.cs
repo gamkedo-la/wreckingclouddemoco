6d4d337fc210895ef968019b22e245973b0f3a50
Chris DeLeon <gamkedo@gmail.com>
2016-07-17 12:16:34 -0700

    Combat Mode scene added

--- Assets/Scripts/CannonBallImpulse.cs
+++ Assets/Scripts/CannonBallImpulse.cs
@@ -6,7 +6,9 @@ public class CannonBallImpulse : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Rigidbody rb = GetComponent<Rigidbody>();
-		rb.AddForce(transform.up * 27000.0f);
-		Destroy(gameObject, 20.0f); // just so they don't pile up forever
+		rb.AddForce((Quaternion.AngleAxis(Random.Range(-5.0f,5.0f),Vector3.right)*
+			Quaternion.AngleAxis(Random.Range(-4.0f,4.0f),Vector3.up)*
+			transform.up) * 2000.0f);
+		Destroy(gameObject, 7.0f); // just so they don't pile up forever
 	}
 }
