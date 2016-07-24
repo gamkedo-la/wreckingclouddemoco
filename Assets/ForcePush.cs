b9d8a1619d985a3b78d287b5fdeefe613c3c0edc
Chris DeLeon <gamkedo@gmail.com>
2016-07-24 02:03:12 -0700

    Fixed bug (seemingly lost code, may've been smashed in a previous branch merge?) which had fusion tank just pushing cores instead of collecting them for player team

--- Assets/ForcePush.cs
+++ Assets/ForcePush.cs
@@ -8,10 +8,16 @@ public class ForcePush : MonoBehaviour {
 		float blastRad = 40.0f;
 		Collider[] list = Physics.OverlapSphere(transform.position, blastRad);
 		int playerLayer = LayerMask.NameToLayer("Player");
+		int goldLayer = LayerMask.NameToLayer("GoldPrize");
+
 		for(int i = 0; i < list.Length; i++) {
-			Rigidbody rb = list[i].GetComponent<Rigidbody>();
-			if(rb && list[i].gameObject.layer != playerLayer) {
-				rb.AddExplosionForce(2000.0f, transform.position, blastRad);
+			if(list[i].gameObject.layer == goldLayer) {
+				GoldGoalTracker.AddPlayerGold(list[i].gameObject);
+			} else {
+				Rigidbody rb = list[i].GetComponent<Rigidbody>();
+				if(rb && list[i].gameObject.layer != playerLayer) {
+					rb.AddExplosionForce(2000.0f, transform.position, blastRad);
+				}
 			}
 		}
 	}
